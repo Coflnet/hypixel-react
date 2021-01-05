78c8f42ddac865f96e6fee5fd3d4591f7475200a
Matthias Luger <malua007@live.de>
2021-01-05 16:04:03 +0100

    use my serviceworker

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -40,7 +40,7 @@ const isLocalhost = Boolean(
       }
   
       window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+        const swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
   
         if (isLocalhost) {
           // This is running on localhost. Let's check if a service worker still exists or not.
