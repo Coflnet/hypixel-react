083623afe166ed45821c1ad531a32a0f8335b98d
Florian Mühlhans <muehlhans.florian@web.de>
2021-01-09 16:38:15 +0100

    cypress wip

---
+++ cypress/integration/search.spec.js
@@ -0,0 +1,25 @@
+
+describe('search', () => {
+
+    // basic search bar
+    it('check search bar', () => {
+        cy.visit('sky.flou21.de')
+        cy.get('input.searchBar')
+            .type('Diamond')
+        cy.get('div.item-details')
+            .should('contain', 'Diamond')
+        cy.get('img.search-result-icon')
+            .first()    
+            .should('be.visible')
+
+        cy.get('button.list-group-item')
+            .first()
+            .click()
+        
+        // check if diagram is visible
+        cy.get('div.graph-canvas-container')
+            .should('be.visible')
+    })
+
+    // check no search result
+})
