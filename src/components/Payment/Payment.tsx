5db8ab4752db03552d1d6af6a582896d26a65002
Florian Mühlhans <muehlhans.florian@web.de>
2021-02-03 21:22:59 +0100

    yet another test

--- src/components/Payment/Payment.tsx
+++ src/components/Payment/Payment.tsx
@@ -7,31 +7,60 @@ const stripePromise = loadStripe(
   "pk_test_51I6N5ZIIRKr1p7dQOGhRRigwIMqgZ3XnoBdbfezFNFgLiR9iaW2YzkRP9kAADCzxSOnqLeqKDVxglDh5uxvY28Dn00vAZR7wQ9"
 );
 
+const PAYMENT_METHOD = "https://play.google.com/billing";
+
 function Payment() {
 
-    let [message, setMessage] = useState("");
+  let [message, setMessage] = useState("");
+
+  const log = (msg: string) => {
+    setMessage(message + msg + '\n');
+  }
+
+  const googleId = () => {
+    return localStorage.getItem('googleId');
+  }
+
+  const checkPaymentPossible = (): boolean => {
+    if (!window.PaymentRequest) {
+      log("No PaymentRequest object.");
+      return false;
+    }
+    return true;
+  }
+
+  const pay = () => {
+    log('Going to pay');
+    log('dont know how to pay..')
+  }
 
   const onPay = async () => {
-    message = '';
-    const googleId = localStorage.getItem('googleId');
-    if ('getDigitalGoodsService' in window) {
-      message += 'digital good service is supported\n'; 
-      const service = await (window as any).getDigitalGoodsService('https://play.google.com/billing');
-      message += JSON.stringify(service) + '\n';
-    } else {
-      message = 'digital good service is not supported\n'; 
+
+    let paymentPossible = checkPaymentPossible()
+
+    if (paymentPossible) {
+      log('payment is possible')
+      pay()
     }
-    setMessage(message);
-  };
+
+  }
+
+  const clearMessages = () => {
+    setMessage('');
+  }
+
 
   return (
     <div>
-    <Button className="btn-success" onClick={onPay}>
-      Buy Premium
+      <Button className="btn-success" onClick={onPay}>
+        Buy Premium
     </Button>
-    <pre>
-      {message}
-    </pre>
+      <Button className="btn-success" onClick={clearMessages}>
+        Clear Messages
+    </Button>
+      <pre>
+        {message}
+      </pre>
     </div>
   )
 }
