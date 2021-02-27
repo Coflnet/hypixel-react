744d500b3244ce908827d1a726c97caf3358c767
Äkwav <xekwavx@gmail.com>
2021-02-27 12:58:57 +0100

    added /cancel

--- src/pages/PaymentCancel/PaymentCancel.tsx
+++ src/pages/PaymentCancel/PaymentCancel.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Cancel() {
     return (
         <div>
-            <h1>Your payment was canceled</h1>
-            <h2>If you still have problems after multiple tries, please contact the developer</h2>
+            <h1>Your canceled the payment process</h1>
+            <h2>If you encountered a problem, feel free to contact us via the <Link to="/feedback">Feedback site</Link></h2>
+            <Link to="/"><Button>Back to front page</Button></Link>
         </div>
     )
 }
