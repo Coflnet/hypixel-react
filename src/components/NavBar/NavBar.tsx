b1c46949fb1da9f30fa6637dc611a93fbf90d1d6
matthias-luger <58751503+matthias-luger@users.noreply.github.com>
2021-06-02 16:57:25 +0200

    Changed to better icons (#131)
    * Changed to better icons
    
    * package-lock created

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,6 +3,7 @@ import './NavBar.css';
 import SideNav from 'react-simple-sidenav';
 import { Link } from 'react-router-dom';
 import { useMatomo } from '@datapunt/matomo-tracker-react';
+import { NotificationsOutlined as NotificationIcon, TrendingUp as TrendIcon, Storefront as StorefrontIcon, AccountBalance as AccountBalanceIcon, Policy as PolicyIcon, Chat as ChatIcon, Menu as MenuIcon } from '@material-ui/icons';
 
 function NavBar() {
 
@@ -20,7 +21,7 @@ function NavBar() {
 
     const navStyle = {
         width: "60%",
-        background: "#3a3f44" 
+        background: "#3a3f44"
     }
 
     const itemStyle = {
@@ -43,20 +44,18 @@ function NavBar() {
                     itemHoverStyle={itemHoverStyle}
                     title={<span>Navigation</span>}
                     items={[
-                        <Link to="/"><div className="link-item-div"><img src="/Coin.png" height="48" width="48" alt="prices icon" loading="lazy" />Prices</div></Link>,
-                        <Link to="/flipper"><div className="link-item-div"><img src="/Coin.png" height="48" width="48" alt="premium icon" loading="lazy" />Item-Flipper</div></Link>,
-                        <Link to="/subscriptions"><div className="link-item-div"><img src="/Coin.png" height="48" width="48" alt="subscription icon" loading="lazy" />Subscriptions</div></Link>,
-                        <Link to="/premium"><div className="link-item-div"><img src="/Coin.png" height="48" width="48" alt="premium icon" loading="lazy" />Premium</div></Link>,
-                        <Link to="/about"><div className="link-item-div"><img src="/Coin.png" height="48" width="48" alt="legal icon" loading="lazy" />Links / Legal</div></Link>,
-                        <Link to="/feedback"><div className="link-item-div"><img src="/Coin.png" height="48" width="48" alt="feedback icon" loading="lazy" />Feedback</div></Link>]}
+                        <Link to="/"><div className="link-item-div"><TrendIcon className="navbar-icon" />Prices</div></Link>,
+                        <Link to="/flipper"><div className="link-item-div"><StorefrontIcon className="navbar-icon" />Item-Flipper</div></Link>,
+                        <Link to="/subscriptions"><div className="link-item-div"><NotificationIcon className="navbar-icon" />Subscriptions</div></Link>,
+                        <Link to="/premium"><div className="link-item-div"><AccountBalanceIcon className="navbar-icon" />Premium</div></Link>,
+                        <Link to="/about"><div className="link-item-div"><PolicyIcon className="navbar-icon" />Links / Legal</div></Link>,
+                        <Link to="/feedback"><div className="link-item-div"><ChatIcon className="navbar-icon" />Feedback</div></Link>]}
                     onHideNav={() => { setShowNav(false) }
                     }>
                 </SideNav >
             </div>
             <span onClick={() => { setShowNav(true); trackEvent({ category: "informations", action: "open" }) }} className="infoIcon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-                </svg>
+                <MenuIcon />
             </span>
         </span >
     );
