3b73ce4e288ed37e3a652beb6b7cbfbf56bf614d
Matthias Luger <malua007@live.de>
2021-06-04 04:22:41 +0200

    small changes

--- src/pages/PaymentSuccess/PaymentSuccess.tsx
+++ src/pages/PaymentSuccess/PaymentSuccess.tsx
@@ -7,9 +7,9 @@ function Success() {
     return (
         <div className="payment-success">
             <Container>
-                <h1 style={{color: "limegreen"}}>Your payment was handled successfully</h1>
+                <h1 style={{color: "limegreen"}}>Your payment was handled successfully!</h1>
                 <p>You should get your features withiin the next few minutes.</p>
-                <p>If any problems should occur please contact us via <Link to="/feedback">the contact page</Link></p>
+                <p>If any problems should occur please contact us via <Link to="/feedback">the contact page</Link>.</p>
                 <Link to="/"><Button>Return to main page</Button></Link>
             </Container>
         </div>
