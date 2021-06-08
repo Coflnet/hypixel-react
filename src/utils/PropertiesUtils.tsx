23ce0a56c5c3f4c7d073b2c96a19069afc8c0a5e
Matthias Luger <malua007@live.de>
2021-06-08 17:59:46 +0200

    changed name to dynamicProps

--- src/utils/PropertiesUtils.tsx
+++ src/utils/PropertiesUtils.tsx
@@ -4,7 +4,7 @@ export function getProperty(propertyName: string) {
 
     // Dynamicly change properties
     if((window as any).debuggingProps && (window as any).debuggingProps[propertyName]){
-        return (window as any).debuggingProps[propertyName];
+        return (window as any).dynamicProps[propertyName];
     }
 
     return properties[propertyName];
