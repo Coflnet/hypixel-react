83b8a839f14266ea43156fd07f68f21aeaac442b
Matthias Luger <malua007@live.de>
2021-06-08 17:58:01 +0200

    enably dynamic change

--- src/utils/PropertiesUtils.tsx
+++ src/utils/PropertiesUtils.tsx
@@ -1,5 +1,11 @@
 import properties from '../properties.json';
 
 export function getProperty(propertyName: string) {
+
+    // Dynamicly change properties
+    if((window as any).debuggingProps && (window as any).debuggingProps[propertyName]){
+        return (window as any).debuggingProps[propertyName];
+    }
+
     return properties[propertyName];
 }
