2c59da46dd69d373b2052464bb6d5949cbc861ff
matthias-luger <58751503+matthias-luger@users.noreply.github.com>
2021-07-10 00:48:58 +0200

    Update src/components/Ref/Ref.tsx
    Co-authored-by: Äkwav <16632490+Ekwav@users.noreply.github.com>

--- src/components/Ref/Ref.tsx
+++ src/components/Ref/Ref.tsx
@@ -62,7 +62,7 @@ function Ref(props: Props) {
                             </p>
                             <hr/>
                             <p>Share your Ref-Link with people which might find skyblock ah history useful. If the invited person logs in with google you and the invited person both get <b>1 free day of premium</b>.</p>
-                            <p>The link above gets to a welcome page for invited users. If you want to share another page on our website and still profit from the Refferal-Program you can add <b style={{whiteSpace: "nowrap"}}>?refId={refInfo?.refId}</b> to the link</p>
+                            <p>The default referral page contains some facts about this site. You are also able to share another page and still get the Referral-Bonus. All you have to do is adding <b style={{whiteSpace: "nowrap"}}>?refId={refInfo?.refId}</b> to any link</p>
                         </Card.Body>
                     </Card>
                 </div>}
