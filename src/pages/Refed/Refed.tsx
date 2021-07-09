410f5482d9d8a94ef4850d0bb91997e18d9228b2
matthias-luger <58751503+matthias-luger@users.noreply.github.com>
2021-07-10 00:50:25 +0200

    Update src/pages/Refed/Refed.tsx
    Co-authored-by: Äkwav <16632490+Ekwav@users.noreply.github.com>

--- src/pages/Refed/Refed.tsx
+++ src/pages/Refed/Refed.tsx
@@ -22,7 +22,6 @@ function Refed() {
                     </Card.Header>
                     <Card.Body>
                         <p>You were invited to use this application because someone thought it would be interesting and helpful to you.</p>
-                        <p>We hope you will enjoy our service.</p>
                         <p>As a welcome gift you will receive 24 hours of free premium to try all features. You can find a list of them as well as your remaining time on the <Link to="/premium">premium page</Link>.</p>
                         <hr />
                         <p>To get access to the free premium day you have to login with google:</p>
