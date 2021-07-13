7ab61f1502991e5bcf37f5b475c4cb4560c23e44
Äkwav <xekwavx@gmail.com>
2021-07-13 17:11:37 +0200

    fix #248

--- src/components/GoogleSignIn/GoogleSignIn.tsx
+++ src/components/GoogleSignIn/GoogleSignIn.tsx
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import api from "../../api/ApiHelper";
 import { refreshTokenSetup } from "../../utils/GoogleUtils";
 import { useHistory } from "react-router-dom";
+import { useMatomo } from "@datapunt/matomo-tracker-react";
 
 interface Props {
     onAfterLogin(): void
@@ -13,6 +14,7 @@ function GoogleSignIn(props: Props) {
 
     let [googleId, setGoogleId] = useState(localStorage.getItem("googleId"));
     let history = useHistory();
+    let { trackEvent } = useMatomo();
 
     useEffect(() => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -43,17 +45,43 @@ function GoogleSignIn(props: Props) {
         });
     };
 
+    const onLoginFail = (response : {error:string,details:string}) => {
+        switch(response.error)
+        {
+            case 'access_denied':
+            case 'popup_closed_by_user':
+                toast.warn("You canceled the login");
+                break;
+            case 'idpiframe_initialization_failed':
+                toast.error("Cookies for accounts.google.com have to be enabled to login",{autoClose:20000});
+                toast.success("Common fix: if there is an eye icon with a line through in your url bar, click it",{delay:5000,autoClose:20000});
+                break;
+        }
+        trackEvent({
+            category: 'login',
+            action: 'error/'+response.error
+        })
+    }
+
+    const onLoginClick = () => {
+        trackEvent({
+            category: 'login',
+            action: 'click'
+        })
+    }
+
     let style: React.CSSProperties = googleId !== null ? {
         visibility: "collapse",
         height: 0
     } : {};
 
     return (
-        <div style={style}>
+        <div style={style} onClickCapture={onLoginClick}>
             <GoogleLogin
                 clientId="570302890760-nlkgd99b71q4d61am4lpqdhen1penddt.apps.googleusercontent.com"
                 buttonText="Login"
                 onSuccess={onLoginSucces}
+                onFailure={onLoginFail}
                 isSignedIn={googleId !== null}
                 theme="dark"
                 cookiePolicy={"single_host_origin"}
