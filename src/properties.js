f23fd3756f77e1ea1e5db075f3cd37836630c165
Flou21 <florian.muehlhans@icloud.com>
2021-07-27 20:43:21 +0200

    removed protocol from hostname check

--- src/properties.js
+++ src/properties.js
@@ -1,5 +1,5 @@
 let properties = {
-    "commandEndpoint": window.location.hostname === 'localhost' || window.location.hostname.includes("https://temp-env-sky-")? "https://sky-preview.coflnet.com/command" : "/command",
+    "commandEndpoint": window.location.hostname === 'localhost' || window.location.hostname.includes("temp-env-sky-")? "https://sky-preview.coflnet.com/command" : "/command",
     "websocketEndpoint": "wss://skyblock-backend.coflnet.com/skyblock",
     "refLink": "https://sky.coflnet.com/refed"
 }
