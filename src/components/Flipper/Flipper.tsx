9156dba54adee9b2adf27b7c70329ee945e9868f
Äkwav <xekwavx@gmail.com>
2021-08-02 23:17:52 +0200

    fix type and hide if irrelevant

--- src/components/Flipper/Flipper.tsx
+++ src/components/Flipper/Flipper.tsx
@@ -355,7 +355,9 @@ function Flipper() {
                                     <span style={{ cursor: "pointer" }}><DeleteIcon color="error" /></span>
                                 </span>
                             </Form.Group>
-                            <p>The flipper is stuck <Tooltip type="hover" content={<HelpIcon />} tooltipContent={<span>We get new auctions every 60sek. from Hypixel. So you may have to wait a bit for a new batch to be found.</span>} /></p>
+                            {hasPremium ?
+                                <p>The flipper is stuck <Tooltip type="hover" content={<HelpIcon />} tooltipContent={<span>We get new auctions every 60 sec. from Hypixel. So you may have to wait a bit for a new ones to be found.</span>} /></p> : ""
+                            }
                         </Form>
                         <hr />
                         {
@@ -381,7 +383,7 @@ function Flipper() {
                     </div>
                 </Card.Body>
                 <Card.Footer>
-                    This flipper is work in progress (proof of concept/open alpha). Anything you see here is subject to change. Please write us your opinion and suggestion on our <a target="_blank" rel="noreferrer" href="https://discord.gg/Qm55WEkgu6">discord</a>.
+                    This flipper is work in progress (open beta). Anything you see here is subject to change. Please write us your opinion and suggestion on our <a target="_blank" rel="noreferrer" href="https://discord.gg/Qm55WEkgu6">discord</a>.
                     <hr />
                     {isLoggedIn ? "" : <span>These are flips that were previosly found (~5 min ago). Anyone can use these and there is no cap on estimated profit.
                         Keep in mind that these are delayed to protect our paying supporters.
