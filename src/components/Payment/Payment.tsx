f56a0f1d34b782c0f497129161faa87b1c442060
Flou21 <florian.muehlhans@icloud.com>
2021-08-11 22:28:58 +0200

    show different text when google / tripe product is displayed

--- src/components/Payment/Payment.tsx
+++ src/components/Payment/Payment.tsx
@@ -136,8 +136,12 @@ function Payment(props: Props) {
                     <div style={{ position: "relative", zIndex: 0 }}>
                       <div id={"paypal-button" + product.itemId}></div>
                     </div> :
+                      product?.paymentProviderName === 'stripe' ?
                     <Button variant="success" onClick={() => { onPay(product) }}>
-                      Buy with credit card
+                        Buy with credit card
+                    </Button> :
+                    <Button variant="success" onClick={() => { onPay(product) }}>
+                        Buy with Google Pay
                     </Button>
                 }
                 {
