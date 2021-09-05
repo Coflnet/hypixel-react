e6e656c77cd66b38db74665faefd5716e33f11df
Matthias Luger <malua007@live.de>
2021-09-05 02:08:14 +0200

    delete all subscriptions

---
+++ src/utils/PremiumReminderUtils.tsx
@@ -0,0 +1,8 @@
+import { toast } from "react-toastify";
+import { wasAlreadyLoggedIn } from "./GoogleUtils";
+
+export default function checkForPremiumReminder(history) {
+    let wasAlreadyLoggedInGoogle = wasAlreadyLoggedIn();
+
+    
+}
