311f51e25ec1bfbeae277f6a7ab63a8ec1d78c93
Matthias Luger <malua007@live.de>
2021-10-08 22:26:48 +0200

    hotfix for payment backend

--- src/properties.js
+++ src/properties.js
@@ -2,6 +2,7 @@ let properties = {
     "commandEndpoint": window.location.host === 'localhost:3000' || window.location.hostname.includes("pr-env-sky-") ? "https://sky-commands.coflnet.com/command" : "/command",
     "apiEndpoint": window.location.host === 'localhost:3000' || window.location.hostname.includes("pr-env-sky-") ? "https://sky-commands.coflnet.com/api" : "/api",
     "websocketEndpoint": "wss://sky-commands.coflnet.com/skyblock",
-    "refLink": "https://sky.coflnet.com/refed"
+    "refLink": "https://sky.coflnet.com/refed",
+    "websocketOldEndpoint": "wss://skyblock-backend.coflnet.com/skyblock"
 }
 export default properties;
