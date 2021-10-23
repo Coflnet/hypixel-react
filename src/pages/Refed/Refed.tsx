33b5197785efbfc6eecd398023f00d603a1f948d
Matthias Luger <malua007@live.de>
2021-10-23 08:16:39 +0200

    Activated refed again Changed text of refed-page

--- src/pages/Refed/Refed.tsx
+++ src/pages/Refed/Refed.tsx
@@ -20,9 +20,8 @@ function Refed() {
                     </Card.Header>
                     <Card.Body>
                         <p>You were invited to use this application because someone thought it would be interesting and helpful to you.</p>
-                        <p>As a welcome gift you will receive 4 hours of free premium to try all features. You can find a list of them as well as your remaining time on the <Link to="/premium">premium page</Link>.</p>
                         <hr />
-                        <p>To get access to the free premium time you have to login with Google:</p>
+                        <p>Login with Google:</p>
                         <GoogleSignIn onAfterLogin={onAfterLogin} />
                         <p>We use Google accounts because they are more secure than requiring a separate login. We use your Google Id and email in compliance with our <a href="https://coflnet.com/privacy">privacy policy</a> (i.e. to know what settings you made and contact you in case we need to)</p>
                         
