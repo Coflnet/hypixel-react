2ab71a34fc9ab71327bb486ad2e5df7e80881979
Äkwav <xekwavx@gmail.com>
2021-12-04 14:09:51 +0100

    use local socket when local

--- src/properties.js
+++ src/properties.js
@@ -1,7 +1,7 @@
 let properties = {
     "commandEndpoint": window.location.host === 'localhost:3000' || window.location.hostname.includes("pr-env-sky-") ? "https://sky.coflnet.com/command" : "/command",
     "apiEndpoint": window.location.host === 'localhost:3000' || window.location.hostname.includes("pr-env-sky-") ? "https://sky.coflnet.com/api" : "/api",
-    "websocketEndpoint": "wss://sky.coflnet.com/skyblock",
+    "websocketEndpoint": window.location.host === 'localhost:8008' ? "ws://localhost:8008/skyblock" : "wss://sky.coflnet.com/skyblock",
     "refLink": "https://sky.coflnet.com/refed",
     "websocketOldEndpoint": "wss://skyblock-backend.coflnet.com/skyblock",
     "feedbackEndpoint": "https://feedback.coflnet.com/api/"
