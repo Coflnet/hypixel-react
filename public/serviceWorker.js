4c65bf7d8618d3a9dcb9b674bc1a37e456da855a
matthias-luger <58751503+matthias-luger@users.noreply.github.com>
2022-02-16 20:52:24 +0100

    removed arc (#689)

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-useless-escape */
 /* eslint-disable no-undef */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
-importScripts("https://arc.io/arc-sw-core.js");
 
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here. Other Firebase libraries
