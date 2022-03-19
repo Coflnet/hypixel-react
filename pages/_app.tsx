6f389cde6fe1f8827d54b5cf9cabe6c7079abfdf
Matthias Luger <malua007@live.de>
2022-03-19 07:14:01 +0100

    removed wrong service worker removed onerror handler

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,31 +9,9 @@ import { useEffect } from 'react'
 import Script from 'next/script'
 import { MainApp } from '../components/MainApp/MainApp'
 import NextNProgress from 'nextjs-progressbar'
-import { isClientSideRendering } from '../utils/SSRUtils'
-
-if (process.env.NODE_ENV === 'production') {
-    if (isClientSideRendering()) {
-        window.onerror = () => {
-            return true
-        }
-    }
-}
 
 function MyApp({ Component, pageProps }) {
     useEffect(() => {
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', function () {
-                navigator.serviceWorker.register('/sw.js').then(
-                    function (registration) {
-                        console.log('Service Worker registration successful with scope: ', registration.scope)
-                    },
-                    function (err) {
-                        console.log('Service Worker registration failed: ', err)
-                    }
-                )
-            })
-        }
-
         window.addEventListener('error', function (event) {
             if (event.error.name === 'ChunkLoadError') {
                 let chunkErrorLocalStorage = window.localStorage.getItem('chunkErrorReload')
