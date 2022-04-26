b6fed379021f08c092ce9880256ed0a85859774c
matthias-luger <58751503+matthias-luger@users.noreply.github.com>
2022-04-26 22:59:32 +0200

    /player now accepts the username (#773)
    * /player now accepts the username
    
    * redirect /p/:username
    
    * remove console.log

---
+++ pages/player/[uuid]/_middleware.tsx
@@ -0,0 +1,17 @@
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
+import api from '../../../api/ApiHelper'
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+    if (req.nextUrl.pathname.startsWith('/player')) {
+        const url = req.nextUrl.clone()
+        let split = url.pathname.split('/')
+        if (split[2].length < 30) {
+            await api.playerSearch(split[2]).then(players => {
+                split[2] = players[0].uuid
+            })
+            url.pathname = split.join('/')
+            return NextResponse.redirect(url)
+        }
+    }
+    return NextResponse.next()
+}
