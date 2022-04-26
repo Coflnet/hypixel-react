e94fa9550d3ba993125c0f44521ad4a4738d05bf
matthias-luger <58751503+matthias-luger@users.noreply.github.com>
2022-04-26 15:01:25 +0200

    allow settings in the toml format to be imported (#766)
    * allow settings in the toml format to be imported
    
    * fixed bug with broken whitelists
    
    * removed useless code
    
    * map user_blacklist to seller blacklist entries

--- utils/Formatter.tsx
+++ utils/Formatter.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties } from "react";
+import { CSSProperties } from 'react'
 
 /*
  Returns a given number as string with thousands-seperators. Example:
@@ -8,141 +8,163 @@ import { CSSProperties } from "react";
 */
 export function numberWithThousandsSeperators(number?: number, seperator?: string): string {
     if (!number) {
-        return "0";
+        return '0'
     }
-    var parts = number.toString().split(".");
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, seperator || ".");
-    return parts.join(",");
+    var parts = number.toString().split('.')
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, seperator || '.')
+    return parts.join(',')
 }
 
 /**
  * Converts a tag (e.g. WOODEN_AXE) to a item name (e.g. Wooden Axe)
  * - replaces all _ with spaces
  * - lowercases the word exept first letter (with exception of the defined words)
- * @param item 
+ * @param item
  */
 export function convertTagToName(itemTag?: string): string {
-
     if (!itemTag) {
-        return "";
+        return ''
     }
 
     // words that should remain lowercase
-    const exceptions = ["of", "the"];
+    const exceptions = ['of', 'the']
 
     function capitalizeWords(text: string): string {
         return text.replace(/\w\S*/g, function (txt) {
             if (exceptions.findIndex(a => a === txt) > -1) {
-                return txt;
+                return txt
             }
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        });
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+        })
     }
 
-    let formatted: string = itemTag.replace(new RegExp("_", "g"), " ").toLowerCase();
+    let formatted: string = itemTag.replace(new RegExp('_', 'g'), ' ').toLowerCase()
 
-
-    formatted = capitalizeWords(formatted);
+    formatted = capitalizeWords(formatted)
     // special per item Formating
-    formatted = formatted?.replace("Pet Item", '');
-    if (formatted?.startsWith("Pet"))
-        formatted = formatted?.replace("Pet", '') + " Pet";
-    if (formatted?.startsWith("Ring"))
-        formatted = formatted?.replace("Ring ", '') + " Ring";
-    return formatted;
+    formatted = formatted?.replace('Pet Item', '')
+    if (formatted?.startsWith('Pet')) formatted = formatted?.replace('Pet', '') + ' Pet'
+    if (formatted?.startsWith('Ring')) formatted = formatted?.replace('Ring ', '') + ' Ring'
+    return formatted
 }
 
 /**
  * Converts a camelCase string (e.g. woodenAxe) to a sentence case (e.g. Wooden axe)
- * @param camelCase 
+ * @param camelCase
  */
 export function camelCaseToSentenceCase(camelCase: string): string {
-
-
-    const exceptions = ["UId"];
+    const exceptions = ['UId']
 
     if (exceptions.findIndex(a => a === camelCase) > -1) {
-        return camelCase;
+        return camelCase
     }
 
-    var result = camelCase.replace(/([A-Z])/g, " $1");
-    var finalResult = result.split(' ');
-    var isFirstWord = true;
+    var result = camelCase.replace(/([A-Z])/g, ' $1')
+    var finalResult = result.split(' ')
+    var isFirstWord = true
     finalResult.forEach((word, i) => {
-        if (word !== "" && isFirstWord) {
-            isFirstWord = false;
-            return;
+        if (word !== '' && isFirstWord) {
+            isFirstWord = false
+            return
         }
-        finalResult[i] = word.toLowerCase();
-    });
-    return finalResult.join(" ");
+        finalResult[i] = word.toLowerCase()
+    })
+    return finalResult.join(' ')
 }
 
 export function getStyleForTier(tier?: string | number): CSSProperties {
-
     interface TierColour {
-        colourCode: string,
+        colourCode: string
         type: string
     }
 
     let tierColors: TierColour[] = [
-        { type: "UNKOWN", colourCode: "black" },
-        { type: "COMMON", colourCode: "black" },
-        { type: "UNCOMMON", colourCode: "#55ff55" },
-        { type: "RARE", colourCode: "#5555ff" },
-        { type: "EPIC", colourCode: "#aa00aa" },
-        { type: "LEGENDARY", colourCode: "#ffaa00" },
-        { type: "SPECIAL", colourCode: "#FF5555" },
-        { type: "VERY_SPECIAL", colourCode: "#FF5555" },
-        { type: "MYTHIC", colourCode: "#ff55ff" },
-        { type: "SUPREME", colourCode: "#AA0000" }]
+        { type: 'UNKOWN', colourCode: 'black' },
+        { type: 'COMMON', colourCode: 'black' },
+        { type: 'UNCOMMON', colourCode: '#55ff55' },
+        { type: 'RARE', colourCode: '#5555ff' },
+        { type: 'EPIC', colourCode: '#aa00aa' },
+        { type: 'LEGENDARY', colourCode: '#ffaa00' },
+        { type: 'SPECIAL', colourCode: '#FF5555' },
+        { type: 'VERY_SPECIAL', colourCode: '#FF5555' },
+        { type: 'MYTHIC', colourCode: '#ff55ff' },
+        { type: 'SUPREME', colourCode: '#AA0000' }
+    ]
 
-
-    let color: TierColour | undefined;
+    let color: TierColour | undefined
 
     if (tier) {
-        //!tier ? DEFAULT_COLOR : (TIER_COLORS[tier.toString().toUpperCase()] || 
+        //!tier ? DEFAULT_COLOR : (TIER_COLORS[tier.toString().toUpperCase()] ||
         if (!isNaN(Number(tier))) {
             color = tierColors[tier]
         } else {
             color = tierColors.find(color => {
-                return color.type === tier.toString().toUpperCase();
+                return color.type === tier.toString().toUpperCase()
             })
         }
     }
 
     return {
         color: color ? color.colourCode : tierColors[0].colourCode,
-        fontFamily: "monospace",
-        fontWeight: "bold"
+        fontFamily: 'monospace',
+        fontWeight: 'bold'
     }
 }
 
 export function enchantmentAndReforgeCompare(a: Enchantment | Reforge, b: Enchantment | Reforge): number {
-    let aName = a.name ? a.name.toLowerCase() : "";
-    let bName = b.name ? b.name.toLowerCase() : "";
+    let aName = a.name ? a.name.toLowerCase() : ''
+    let bName = b.name ? b.name.toLowerCase() : ''
 
-    if (aName === "any" || (aName === "none" && bName !== "any")) {
-        return -1;
+    if (aName === 'any' || (aName === 'none' && bName !== 'any')) {
+        return -1
     }
-    if (bName === "any" || bName === "none") {
-        return 1;
+    if (bName === 'any' || bName === 'none') {
+        return 1
     }
 
-    return aName.localeCompare(bName);
+    return aName.localeCompare(bName)
 }
 
 export function formatToPriceToShorten(num: number, decimals: number = 0): string {
     // Ensure number has max 3 significant digits (no rounding up can happen)
-    let i = Math.pow(10, Math.max(0, Math.log10(num) - 2));
-    num = num / i * i;
+    let i = Math.pow(10, Math.max(0, Math.log10(num) - 2))
+    num = (num / i) * i
 
-    if (num >= 1_000_000_000)
-        return (num / 1_000_000_000).toFixed(decimals) + "B";
-    if (num >= 1_000_000)
-        return (num / 1_000_000).toFixed(decimals) + "M";
-    if (num >= 1_000)
-        return (num / 1_000).toFixed(decimals) + "k";
+    if (num >= 1_000_000_000) return (num / 1_000_000_000).toFixed(decimals) + 'B'
+    if (num >= 1_000_000) return (num / 1_000_000).toFixed(decimals) + 'M'
+    if (num >= 1_000) return (num / 1_000).toFixed(decimals) + 'k'
 
     return num.toFixed(0)
+}
+
+/**
+ * Returs a number from a short represantation string of a price (e.g. 12M => 12_000_000)
+ * @param shortString A string representing a larger number (e.g. 12M)
+ * @returns The number represented by the string (e.g. 12_000_000)
+ */
+export function getNumberFromShortenString(shortString?: string): number | undefined {
+    if (!shortString) {
+        return
+    }
+    let split
+    let multiplier
+    if (shortString.indexOf('B') !== -1) {
+        split = shortString.split('B')
+        multiplier = 1000000000
+    }
+    if (shortString.indexOf('M') !== -1) {
+        split = shortString.split('M')
+        multiplier = 1000000
+    }
+    if (shortString.indexOf('K') !== -1) {
+        split = shortString.split('K')
+        multiplier = 1000
+    }
+    if (!split) {
+        split = [shortString]
+        multiplier = 1
+    }
+    if (split[0] && !isNaN(+split[0])) {
+        return parseInt(split[0]) * multiplier
+    }
 }
