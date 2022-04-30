407514cefd565f51ddb2ce40adfb66e168411437
Matthias Luger <malua007@live.de>
2022-04-30 14:14:46 +0200

    added echarts

--- components/PriceGraph/PriceGraph.tsx
+++ components/PriceGraph/PriceGraph.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef, useState } from 'react'
-import Chart, { ChartConfiguration } from 'chart.js'
 import api from '../../api/ApiHelper'
-import priceConfig from './PriceGraphConfig'
+import graphConfig from './PriceGraphConfig'
 import { DateRange, DEFAULT_DATE_RANGE, ItemPriceRange } from '../ItemPriceRange/ItemPriceRange'
 import { getLoadingElement } from '../../utils/LoadingUtils'
 import { numberWithThousandsSeperators } from '../../utils/Formatter'
@@ -14,6 +13,7 @@ import { getItemFilterFromUrl } from '../../utils/Parser/URLParser'
 import ActiveAuctions from '../ActiveAuctions/ActiveAuctions'
 import { isClientSideRendering } from '../../utils/SSRUtils'
 import styles from './PriceGraph.module.css'
+import ReactECharts from 'echarts-for-react'
 
 interface Props {
     item: Item
@@ -25,8 +25,6 @@ let currentLoadingString
 let mounted = true
 
 function PriceGraph(props: Props) {
-    const priceChartCanvas = useRef<HTMLCanvasElement>(null)
-    let [priceChart, setPriceChart] = useState<Chart>()
     let [fetchspan, setFetchspan] = useState(DEFAULT_DATE_RANGE)
     let [isLoading, setIsLoading] = useState(false)
     let [noDataFound, setNoDataFound] = useState(false)
@@ -35,6 +33,7 @@ function PriceGraph(props: Props) {
     let [itemFilter, setItemFilter] = useState<ItemFilter>()
     let [isItemFilterPrefill, setIsItemFilterPrefill] = useState<boolean>(true)
     let [defaultRangeSwitch, setDefaultRangeSwitch] = useState(true)
+    let [chartOptions, setChartOptions] = useState(graphConfig)
 
     let fetchspanRef = useRef(fetchspan)
     fetchspanRef.current = fetchspan
@@ -56,20 +55,15 @@ function PriceGraph(props: Props) {
     useEffect(() => {
         fetchspan = DEFAULT_DATE_RANGE
         setFetchspan(DEFAULT_DATE_RANGE)
-        if (priceChartCanvas && priceChartCanvas.current) {
-            if (Object.keys(getItemFilterFromUrl()).length === 0) {
-                setIsItemFilterPrefill(false)
-                let chart = priceChart || createChart(priceConfig)
-                priceChart = chart
-                setPriceChart(chart)
-                if (props.item) {
-                    updateChart(chart, fetchspan, undefined)
-                }
+        if (Object.keys(getItemFilterFromUrl()).length === 0) {
+            setIsItemFilterPrefill(false)
+            if (props.item) {
+                updateChart(fetchspan, undefined)
             }
         }
     }, [props.item.tag])
 
-    let updateChart = (priceChart: Chart, fetchspan: DateRange, itemFilter?: ItemFilter) => {
+    let updateChart = (fetchspan: DateRange, itemFilter?: ItemFilter) => {
         // active auction is selected
         // no need to get new price data
         if (fetchspan === DateRange.ACTIVE) {
@@ -78,10 +72,12 @@ function PriceGraph(props: Props) {
         }
 
         setIsLoading(true)
-        priceChart.data.labels = []
-        priceChart.data.datasets![0].data = []
-        priceChart.update()
-        setPriceChart(priceChart)
+
+        chartOptions.xAxis[0].data = []
+        chartOptions.series[0].data = []
+        chartOptions.series[1].data = []
+        chartOptions.series[2].data = []
+        chartOptions.series[3].data = []
 
         currentLoadingString = JSON.stringify({
             tag: props.item.tag,
@@ -103,33 +99,29 @@ function PriceGraph(props: Props) {
                     return
                 }
 
-                priceChart!.data.labels = prices.map(item => item.time.getTime())
-                priceChart!.data.labels = priceChart!.data!.labels!.sort((a, b) => {
+                chartOptions.xAxis[0].data = prices.map(item => item.time.getTime())
+                chartOptions.xAxis[0].data = chartOptions.xAxis[0].data.sort((a, b) => {
                     return (a as number) - (b as number)
                 })
 
                 let priceSum = 0
-                priceChart!.data.datasets![0].data = []
-                priceChart!.data.datasets![1].data = []
-                priceChart!.data.datasets![2].data = []
-                priceChart!.data.datasets![3].data = []
 
                 prices.forEach(item => {
                     priceSum += item.avg
-                    priceChart!.data!.datasets![0].data!.push(item.avg)
-                    priceChart!.data!.datasets![1].data!.push(item.min)
-                    priceChart!.data!.datasets![2].data!.push(item.max)
-                    priceChart!.data!.datasets![3].data!.push(item.volume)
+                    chartOptions.series[0].data.push(item.avg)
+                    chartOptions.series[1].data.push(item.min)
+                    chartOptions.series[2].data.push(item.max)
+                    chartOptions.series[3].data.push(item.volume)
                 })
 
-                priceChart.update()
                 setAvgPrice(Math.round(priceSum / prices.length))
-                setPriceChart(priceChart)
                 setNoDataFound(prices.length === 0)
                 setIsLoading(false)
                 setTimeout(() => {
                     setIsItemFilterPrefill(false)
                 }, 100)
+
+                setChartOptions(chartOptions)
             })
             .catch(() => {
                 setIsLoading(false)
@@ -138,14 +130,10 @@ function PriceGraph(props: Props) {
             })
     }
 
-    let createChart = (chartConfig: ChartConfiguration): Chart => {
-        return new Chart(priceChartCanvas.current as HTMLCanvasElement, chartConfig)
-    }
-
     let onRangeChange = (timespan: DateRange) => {
         setFetchspan(timespan)
-        if (priceChart && timespan !== DateRange.ACTIVE) {
-            updateChart(priceChart!, timespan, itemFilter)
+        if (timespan !== DateRange.ACTIVE) {
+            updateChart(timespan, itemFilter)
         }
     }
 
@@ -153,7 +141,7 @@ function PriceGraph(props: Props) {
         setItemFilter(filter)
         setDefaultRangeSwitch(!defaultRangeSwitch)
         if (fetchspanRef.current !== DateRange.ACTIVE) {
-            updateChart(priceChart || createChart(priceConfig), fetchspanRef.current, filter)
+            updateChart(fetchspanRef.current, filter)
         }
     }
 
@@ -162,18 +150,6 @@ function PriceGraph(props: Props) {
             setFilters(filters)
         })
     }
-
-    let graphOverlayElement = isLoading ? (
-        <div className={styles.graphOverlay}>{getLoadingElement()}</div>
-    ) : noDataFound && !isLoading ? (
-        <div className={styles.graphOverlay}>
-            <div style={{ textAlign: 'center' }}>
-                <p>No data found</p>
-            </div>
-        </div>
-    ) : (
-        ''
-    )
 
     return (
         <div>
@@ -186,10 +162,7 @@ function PriceGraph(props: Props) {
             />
 
             <div style={fetchspan === DateRange.ACTIVE ? { display: 'none' } : {}}>
-                <div className={styles.graphCanvasContainer}>
-                    {graphOverlayElement}
-                    <canvas ref={priceChartCanvas} />
-                </div>
+                {chartOptions.xAxis[0].data.length > 0 ? <ReactECharts option={chartOptions} className={styles.chart} /> : null}
                 <div className={styles.additionalInfos}>
                     <span className={styles.avgPrice}>
                         <b>Avg Price:</b> {isLoading ? '-' : numberWithThousandsSeperators(avgPrice) + ' Coins'}
--- components/PriceGraph/PriceGraphConfig.tsx
+++ components/PriceGraph/PriceGraphConfig.tsx
@@ -1,29 +1,30 @@
-import { ChartConfiguration } from "chart.js";
-import { numberWithThousandsSeperators } from "../../utils/Formatter";
+import { ChartConfiguration } from 'chart.js'
+import moment from 'moment'
+import { numberWithThousandsSeperators } from '../../utils/Formatter'
 
-export let data: ChartConfiguration = {
-    type: "line",
+let data: ChartConfiguration = {
+    type: 'line',
     data: {
         datasets: [
             {
-                label: "price",
-                borderColor: "#22A7F0",
+                label: 'price',
+                borderColor: '#22A7F0',
                 fill: false
             },
             {
-                label: "min",
-                borderColor: "#228B22",
+                label: 'min',
+                borderColor: '#228B22',
                 fill: false
             },
             {
-                label: "max",
-                borderColor: "#B22222",
+                label: 'max',
+                borderColor: '#B22222',
                 hidden: true,
                 fill: false
             },
             {
-                label: "number of items traded",
-                borderColor: "#000000",
+                label: 'number of items traded',
+                borderColor: '#000000',
                 fill: true
             }
         ]
@@ -31,31 +32,31 @@ export let data: ChartConfiguration = {
     options: {
         maintainAspectRatio: false,
         title: {
-            display: false,
+            display: false
         },
         animation: {
             duration: 0
         },
         tooltips: {
-            mode: "index",
+            mode: 'index',
             intersect: false,
             callbacks: {
                 label: function (tooltipItem, data): string {
                     if (tooltipItem.datasetIndex) {
-                        let dataSetName = data.datasets![tooltipItem.datasetIndex!].label;
+                        let dataSetName = data.datasets![tooltipItem.datasetIndex!].label
                         if (dataSetName === data.datasets![3].label) {
-                            return numberWithThousandsSeperators(Math.round(parseInt(tooltipItem.value || "0"))).toString() + " items sold"
+                            return numberWithThousandsSeperators(Math.round(parseInt(tooltipItem.value || '0'))).toString() + ' items sold'
                         }
                     }
 
-                    return numberWithThousandsSeperators(Math.round(parseInt(tooltipItem.value || "0"))).toString() + " coins";
+                    return numberWithThousandsSeperators(Math.round(parseInt(tooltipItem.value || '0'))).toString() + ' coins'
                 },
                 title: function (tooltipItem, data) {
                     if (tooltipItem[0] && tooltipItem[0].label) {
-                        let d = new Date(tooltipItem[0].label);
-                        return d.toLocaleTimeString() + ", " + d.toLocaleDateString();
+                        let d = new Date(tooltipItem[0].label)
+                        return d.toLocaleTimeString() + ', ' + d.toLocaleDateString()
                     } else {
-                        return "-";
+                        return '-'
                     }
                 }
             }
@@ -71,18 +72,115 @@ export let data: ChartConfiguration = {
                     ticks: {
                         beginAtZero: true,
                         callback: function (value, index, values) {
-                            return numberWithThousandsSeperators(Math.round(parseInt(value.toString())));
+                            return numberWithThousandsSeperators(Math.round(parseInt(value.toString())))
                         }
                     }
                 }
             ],
             xAxes: [
                 {
-                    type: "time"
+                    type: 'time'
                 }
             ]
         }
     }
 }
 
-export default data;
+let option = {
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'cross',
+            crossStyle: {
+                color: '#999'
+            }
+        }
+    },
+    legend: {
+        data: ['Price', 'Min', 'Max', 'Number of traded items'],
+        selected: {
+            Price: true,
+            Min: true,
+            Max: false,
+            'Number of traded items': false
+        }
+    },
+    xAxis: [
+        {
+            type: 'category',
+            data: [],
+            axisPointer: {
+                type: 'shadow'
+            },
+            axisLabel: {
+                formatter: value => `${new Date(+value).toLocaleDateString()}`
+            }
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value',
+            name: 'Price',
+            position: 'left',
+            axisLabel: {
+                formatter: value => `${numberWithThousandsSeperators(value)} Coins`
+            }
+        },
+        {
+            type: 'value',
+            name: 'Number of traded items',
+            position: 'right',
+            axisLabel: {
+                formatter: value => `${numberWithThousandsSeperators(value)}`
+            }
+        }
+    ],
+    series: [
+        {
+            name: 'Price',
+            type: 'line',
+            color: '#22A7F0',
+            yAxisIndex: 0,
+            smooth: true,
+            tooltip: {
+                valueFormatter: value => `${numberWithThousandsSeperators(value)} Coins`
+            },
+        },
+        {
+            name: 'Min',
+            type: 'line',
+            color: '#228B22',
+            yAxisIndex: 0,
+            smooth: true,
+            tooltip: {
+                valueFormatter: value => `${numberWithThousandsSeperators(value)} Coins`
+            },
+            data: []
+        },
+        {
+            name: 'Max',
+            type: 'line',
+            color: '#B22222',
+            yAxisIndex: 0,
+            smooth: true,
+            tooltip: {
+                valueFormatter: value => `${numberWithThousandsSeperators(value)} Coins`
+            },
+            data: []
+        },
+        {
+            name: 'Number of traded items',
+            type: 'bar',
+            color: '#000000',
+            areaStyle: {},
+            yAxisIndex: 1,
+            smooth: true,
+            tooltip: {
+                valueFormatter: value => `${numberWithThousandsSeperators(value)}`
+            },
+            data: []
+        }
+    ]
+}
+
+export default option
