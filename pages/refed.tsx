3d6e97c3ce155f5082de62486a96036a1bc742c8
Matthias Luger <malua007@live.de>
2022-05-16 05:09:24 +0200

    updated refed page

--- pages/refed.tsx
+++ pages/refed.tsx
@@ -17,13 +17,23 @@ function Refed() {
                     </Card.Header>
                     <Card.Body>
                         <p>You were invited to use this application because someone thought it would be interesting and helpful to you.</p>
+                        <p>
+                            After you are logged in with Google you can verify your Minecraft account to get 1 hour of <Link href="/premium">premium</Link> for
+                            free to test our service including our <Link href="/flipper">auction flipper</Link>.
+                        </p>
+                        <p>
+                            We also provide a mod to use the flipper ingame. You can download it in the <b>#mod-releases</b> channel on our{' '}
+                            <a href="https://discord.gg/wvKXfTgCfb">
+                                <span style={{ color: '#7289da' }}>Discord</span>
+                            </a>
+                            . For help check out the <b>#faq</b> channel or ask in <b>#support</b>.
+                        </p>
                         <hr />
                         <p>Login with Google:</p>
                         <GoogleSignIn onAfterLogin={() => {}} />
                         <p>
-                            We use Google accounts because they are more secure than requiring a separate login. We use your Google Id and email in compliance
-                            with our <a href="https://coflnet.com/privacy">privacy policy</a> (i.e. to know what settings you made and contact you in case we
-                            need to)
+                            We use Google accounts because they are more secure than requiring a separate login. We use your Google Id and email. (i.e. to know
+                            what settings you made and contact you in case we need to)
                         </p>
 
                         <Link href="/">
