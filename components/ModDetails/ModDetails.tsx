2be589fe3617e1edb8c92485a99dbbf8a1e8e8e2
Matthias Luger <malua007@live.de>
2022-08-09 00:40:42 +0200

    added mod github link

--- components/ModDetails/ModDetails.tsx
+++ components/ModDetails/ModDetails.tsx
@@ -12,6 +12,20 @@ function ModDetails() {
                     <p>We provide a mod as a quality of life feature to use the flipper in minecraft directly.</p>
                     <p>To use the mod you will need to link it to the website and verify your Minecraft account.</p>
                     <hr />
+                    <p>
+                        The only legit place to download is on our{' '}
+                        <a target="_blank" rel="noreferrer" href="https://discord.gg/wvKXfTgCfb">
+                            Discord
+                        </a>{' '}
+                        in the #mod-releases channel.
+                    </p>
+                    <p>
+                        The entire mod is open source and you can check/compile it yourself here:{' '}
+                        <a target="_blank" rel="noreferrer" href="https://github.com/Coflnet/SkyblockMod">
+                            https://github.com/Coflnet/SkyblockMod
+                        </a>
+                    </p>
+                    <hr />
                     <div style={{ display: 'flex', justifyContent: 'center' }}>
                         <iframe
                             width="560"
