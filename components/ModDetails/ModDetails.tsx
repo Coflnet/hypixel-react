eabfa9d6a91c8e992312fa3cf89c1178c644e448
matthias-luger <58751503+matthias-luger@users.noreply.github.com>
2022-08-24 19:03:36 +0200

    Update components/ModDetails/ModDetails.tsx
    Co-authored-by: Äkwav <16632490+Ekwav@users.noreply.github.com>

--- components/ModDetails/ModDetails.tsx
+++ components/ModDetails/ModDetails.tsx
@@ -9,8 +9,14 @@ function ModDetails() {
                     <Card.Title>Details</Card.Title>
                 </Card.Header>
                 <Card.Body>
-                    <p>We provide a mod as a quality of life feature to use the flipper in minecraft directly.</p>
-                    <p>To use the mod you will need to link it to the website and verify your Minecraft account.</p>
+                    <p>Say goodbye to window switching. The cofl mod provides you with data directly in Hypixel Skyblock. You can access price paid, the ah flipper, median, lbin, bazaar prices and more.</p>
+                    <h3>Installation TLDR
+                    <ol>
+                    <li>Get and install Forge 1.8.9 (if you don't have already watch the video below)</li>
+                    <li>Download and put the mod into your mods folder</li>
+                    <li>Launch Minecraft and join Skyblock</li>
+                    <li>Get a list of commands with /cofl help or watch the <a href="https://www.youtube.com/watch?v=dv66Y_cra8U&list=PLDpPmxIcq9tAssQlyJMBlSmSg5JOpq699">YouTube playlist</a> explaining them</li>
+                    </ol>
                     <hr />
                     <p>
                         The only legit place to download is on our{' '}
