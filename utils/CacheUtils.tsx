0a11f1bb1384bd5b49b42d4f11a85618d5ad320c
matthias-luger <58751503+matthias-luger@users.noreply.github.com>
2022-09-01 01:48:27 +0200

    Update utils/CacheUtils.tsx
    Co-authored-by: Äkwav <16632490+Ekwav@users.noreply.github.com>

--- utils/CacheUtils.tsx
+++ utils/CacheUtils.tsx
@@ -73,7 +73,7 @@ let cacheUtils: CacheUtils = {
 export default cacheUtils
 
 export function getCacheContolHeader() {
-    return 'public, max-age=300, s-maxage=60, stale-while-revalidate=59'
+    return 'public, max-age=60, s-maxage=20, stale-while-revalidate=19'
 }
 
 interface CacheEntry {
