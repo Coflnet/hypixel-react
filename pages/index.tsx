65d55f5cd0d70bee58e995488abc7f5390e71a7a
Matthias Luger <malua007@live.de>
2022-09-01 02:17:03 +0200

    add cache header to start page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Search from '../components/Search/Search'
 import { initAPI } from '../api/ApiHelper'
 import { parseAuction, parseItem, parsePlayer, parsePopularSearch } from '../utils/Parser/APIResponseParser'
 import { getHeadElement } from '../utils/SSRUtils'
+import { getCacheContolHeader } from '../utils/CacheUtils'
 
 interface Props {
     newAuctions: Auction[]
@@ -32,7 +33,9 @@ const Startpage = (props: Props) => {
     )
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async res => {
+    res.setHeader('Cache-Control', getCacheContolHeader())
+
     let api = initAPI(true)
     // Dont load ended Auctions, as this is a expensive computation and can take multiple seconds
     let results = await Promise.all([api.getNewAuctions(), api.getNewPlayers(), api.getPopularSearches(), api.getNewItems()].map(p => p.catch(e => null)))
