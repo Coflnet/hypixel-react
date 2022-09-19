ab79d1fce779c08b55cd3ddcd77ac2570bf52ea9
Matthias Luger <malua007@live.de>
2022-09-20 01:35:07 +0200

    copy link to auction after clicking notifier

--- utils/NotificationUtils.tsx
+++ utils/NotificationUtils.tsx
@@ -1,36 +1,36 @@
-import { toast } from "react-toastify";
-import cacheUtils from "./CacheUtils";
+import { toast } from 'react-toastify'
+import cacheUtils from './CacheUtils'
 
 export default function registerNotificationCallback(router) {
     let interval = setInterval(function () {
         // wait until messaging is definded
-        let messaging = (window as any).messaging;
-        if (typeof messaging == 'undefined') return;
-        clearInterval(interval);
+        let messaging = (window as any).messaging
+        if (typeof messaging == 'undefined') return
+        clearInterval(interval)
 
         messaging.onMessage(function (payload) {
-            let notification = payload.notification;
-            if (payload.data.type === "auction") {
-                savePayloadIntoCache(payload);
+            let notification = payload.notification
+            if (payload.data.type === 'auction') {
+                savePayloadIntoCache(payload)
             }
-            displayNotification(notification);
-        });
-    }, 10);
+            displayNotification(notification)
+        })
+    }, 10)
 
     function displayNotification(notification: any) {
-        console.log(notification)
-        toast.info(notification.title + "\n" + notification.body, {
+        toast.info(notification.title + '\n' + notification.body, {
             onClick: () => {
-                router.push(
-                    '/' + notification.click_action.match(/\/\/[^/]+\/([^.]+)/)[1]
-                );
+                if (window.navigator.clipboard) {
+                    window.navigator.clipboard.writeText(notification.click_action)
+                }
+                router.push('/' + notification.click_action.match(/\/\/[^/]+\/([^.]+)/)[1])
             },
             autoClose: 20000
-        });
+        })
     }
 
     function savePayloadIntoCache(payload: any) {
-        let auction = JSON.parse(payload.data.auction);
-        cacheUtils.setIntoCache("auctionDetails", JSON.stringify(auction.uuid), auction, 60);
+        let auction = JSON.parse(payload.data.auction)
+        cacheUtils.setIntoCache('auctionDetails', JSON.stringify(auction.uuid), auction, 60)
     }
 }
