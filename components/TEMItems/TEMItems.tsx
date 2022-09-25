996a49621fe71c6165bd86ec000e0a3bad81abfa
Matthias Luger <malua007@live.de>
2022-09-25 20:57:41 +0200

    added option to select profile for tem items

--- components/TEMItems/TEMItems.tsx
+++ components/TEMItems/TEMItems.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import { Modal } from 'react-bootstrap'
+import React, { ChangeEvent, useEffect, useState } from 'react'
+import { Form, Modal } from 'react-bootstrap'
 import api from '../../api/ApiHelper'
 import { getStyleForTier } from '../../utils/Formatter'
+import { getLoadingElement } from '../../utils/LoadingUtils'
 import TEMInventory from './TEMInventory/TEMInventory'
 import TEMItemDetails from './TEMItemDetails/TEMItemDetails'
 
@@ -13,10 +14,19 @@ function TEMItems(props: Props) {
     let [playerData, setPlayerData] = useState<TEM_Player>()
     let [detailEntry, setDetailEntry] = useState<TEM_Item | TEM_Pet>()
     let [detailEntryType, setDetailEntryType] = useState<'pet' | 'item'>()
+    let [profiles, setProfiles] = useState<SkyblockProfile[]>()
+    let [selectedProfile, setSelectedProfile] = useState<SkyblockProfile>()
+    let [isLoading, setIsLoading] = useState<boolean>(true)
 
     useEffect(() => {
-        api.getTEMPlayerData(props.playerUUID).then(data => {
-            setPlayerData(data)
+        api.getPlayerProfiles(props.playerUUID).then(profiles => {
+            let selectedProfile = profiles.find(profile => profile.current)
+            setSelectedProfile(selectedProfile)
+            setProfiles(profiles)
+            api.getTEMPlayerDataByProfileUUID(selectedProfile.id).then(playerData => {
+                setPlayerData(playerData)
+                setIsLoading(false)
+            })
         })
     }, [])
 
@@ -42,6 +52,18 @@ function TEMItems(props: Props) {
         ))
     }
 
+    function onProfileChange(event: ChangeEvent<HTMLSelectElement>) {
+        let selectedIndex = event.target.options.selectedIndex
+        let value = event.target.options[selectedIndex].getAttribute('value')!
+        let newSelectedProfile = profiles?.find(p => p.cuteName === value)
+        setSelectedProfile(newSelectedProfile)
+        setIsLoading(true)
+        api.getTEMPlayerDataByProfileUUID(newSelectedProfile.id).then(playerData => {
+            setPlayerData(playerData)
+            setIsLoading(false)
+        })
+    }
+
     let detailDialog = !!detailEntry ? (
         <Modal
             size={'xl'}
@@ -58,13 +80,32 @@ function TEMItems(props: Props) {
             </Modal.Body>
         </Modal>
     ) : null
-
     return (
         <>
-            {playerData ? getEntryList(playerData.items, 'items') : null}
-            <hr />
-            {playerData ? getEntryList(playerData.pets, 'pets') : null}
-            {detailDialog}
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                {profiles ? (
+                    <p>
+                        <span style={{ float: 'left', marginRight: '15px' }}>Profile: </span>
+                        <Form.Control style={{ width: 'auto' }} defaultValue={selectedProfile?.cuteName} as="select" onChange={onProfileChange}>
+                            {profiles.map(profile => (
+                                <option key={profile.cuteName} value={profile.cuteName}>
+                                    {profile.cuteName}
+                                </option>
+                            ))}
+                        </Form.Control>
+                    </p>
+                ) : null}
+            </div>
+            {isLoading ? (
+                getLoadingElement()
+            ) : (
+                <div>
+                    {playerData ? getEntryList(playerData.items, 'items') : null}
+                    <hr />
+                    {playerData ? getEntryList(playerData.pets, 'pets') : null}
+                    {detailDialog}
+                </div>
+            )}
         </>
     )
 }
