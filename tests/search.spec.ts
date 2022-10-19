48dc89a3d93a7f82f37f4d4ff26730ea94a266f4
Äkwav <ekwav@coflnet.com>
2022-10-19 12:49:50 +0200

    add test

---
+++ tests/search.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+test('test', async ({ page }) => {
+  await page.goto('https://sky.coflnet.com/');
+  await page.getByPlaceholder('Search player/item').click();
+  await page.getByPlaceholder('Search player/item').fill('technoblade');
+  await page.getByPlaceholder('Search player/item').press('Enter');
+  await expect(page).toHaveURL('https://sky.coflnet.com/player/b876ec32e396476ba1158438d83c67d4');
+  await page.getByRole('heading', { name: 'TechnobladeYou? Claim account. Check tracked flips' }).getByText('Technoblade').click();
+});
