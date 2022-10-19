e57ef0d65580a3cd00544673cea95a325f2513fc
Äkwav <ekwav@coflnet.com>
2022-10-19 13:31:29 +0200

    use playwright.config

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
-const basUrl = "http://localhost:3000";
+
 test('test', async ({ page }) => {
-  await page.goto(basUrl);
+  await page.goto('/');
   await page.getByPlaceholder('Search player/item').click();
   await page.getByPlaceholder('Search player/item').fill('technoblade');
   await page.getByPlaceholder('Search player/item').press('Enter');
-  await expect(page).toHaveURL(basUrl+'/player/b876ec32e396476ba1158438d83c67d4');
+  await expect(page).toHaveURL('/player/b876ec32e396476ba1158438d83c67d4');
   await page.getByRole('heading', { name: 'TechnobladeYou? Claim account. Check tracked flips' }).getByText('Technoblade').click();
 });
