8fa121f87798f348a46e27398e39244b61fb9515
Äkwav <ekwav@coflnet.com>
2022-10-19 13:21:13 +0200

    fix expected url

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
+const basUrl = "http://localhost:3000";
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto(basUrl);
   await page.getByPlaceholder('Search player/item').click();
   await page.getByPlaceholder('Search player/item').fill('technoblade');
   await page.getByPlaceholder('Search player/item').press('Enter');
-  await expect(page).toHaveURL('https://sky.coflnet.com/player/b876ec32e396476ba1158438d83c67d4');
+  await expect(page).toHaveURL(basUrl+'/player/b876ec32e396476ba1158438d83c67d4');
   await page.getByRole('heading', { name: 'TechnobladeYou? Claim account. Check tracked flips' }).getByText('Technoblade').click();
 });
