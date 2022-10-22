9fefef59d081d166d667825f02e380f27ee0bd4c
Äkwav <ekwav@coflnet.com>
2022-10-22 12:07:18 +0200

    remembered special option to force player search

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('search player technoblade', async ({ page }) => {
   await page.goto('/');
   await page.getByPlaceholder('Search player/item').click();
-  await page.getByPlaceholder('Search player/item').fill('technoblade');
+  await page.getByPlaceholder('Search player/item').fill('playertechnoblade');
   await page.getByPlaceholder('Search player/item').press('Enter');
   await expect(page).toHaveURL('/player/b876ec32e396476ba1158438d83c67d4');
   await page.getByRole('heading', { name: 'TechnobladeYou? Claim account. Check tracked flips' }).getByText('Technoblade').click();
