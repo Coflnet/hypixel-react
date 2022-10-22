ce64d63e3ff05de9858e564640fe59565d8222f8
Matthias Luger <malua007@live.de>
2022-10-22 13:17:56 +0200

    update test title

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,10 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-test('search player technoblade', async ({ page }) => {
+test('search player technoblade with special player search query', async ({ page }) => {
   await page.goto('/');
   await page.getByPlaceholder('Search player/item').click();
   await page.getByPlaceholder('Search player/item').fill('playertechnoblade');
   await page.getByPlaceholder('Search player/item').press('Enter');
   await expect(page).toHaveURL('/player/b876ec32e396476ba1158438d83c67d4');
-  await page.getByRole('heading', { name: 'TechnobladeYou? Claim account. Check tracked flips' }).getByText('Technoblade').click();
 });
