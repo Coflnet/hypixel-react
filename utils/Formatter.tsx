fd396accd358033c5b8d2af64f0a219c2e63eb13
Matthias Luger <malua007@live.de>
2022-11-27 22:25:16 +0100

    fix typo

--- utils/Formatter.tsx
+++ utils/Formatter.tsx
@@ -78,7 +78,7 @@ export function getStyleForTier(tier?: string | number): CSSProperties {
     }
 
     let tierColors: TierColour[] = [
-        { type: 'UNKOWN', colourCode: 'black' },
+        { type: 'UNKNOWN', colourCode: 'black' },
         { type: 'COMMON', colourCode: 'black' },
         { type: 'UNCOMMON', colourCode: '#55ff55' },
         { type: 'RARE', colourCode: '#5555ff' },
