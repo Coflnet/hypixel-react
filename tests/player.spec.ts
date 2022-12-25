69fab426a276704eb087ba610d01047d860a0921
matthias-luger <58751503+matthias-luger@users.noreply.github.com>
2022-12-26 00:25:03 +0100

    fixed ownhighestbid in tests (#1010)

--- tests/player.spec.ts
+++ tests/player.spec.ts
@@ -9,7 +9,7 @@ test('Player auction opens', async ({ page }) => {
 test('Opens last bid', async ({ page }) => {
   await page.goto('https://sky.coflnet.com/player/b876ec32e396476ba1158438d83c67d4');
   await switchToBids(page);
-  await page.getByRole('link', { name: /.*Jingle Bells Ended BIN Highest Bid: 620[,\.]000.* Highest Own: 0.* End of Auction: .*/i }).click();
+  await page.getByRole('link', { name: /.*Jingle Bells Ended BIN Highest Bid: 620[,\.]000.* Highest Own: 620[,\.]000.* End of Auction: .*/i }).click();
   await expect(page).toHaveURL(/.*\/auction\/c5ce8b40320b4b178e53cdfb746d8953/i);
 });
 
@@ -19,12 +19,12 @@ test('Scroll down to older bid', async ({ page, isMobile, browserName }) => {
   await switchToBids(page);
   for (let i = 0; i < 500; i++) {
     await page.mouse.wheel(0, 5000);
-    var inView = await page.getByRole('link', { name: /.*Cheap Coffee Ended BIN Highest Bid: 6[,\.]000.* Highest Own: 0.* End of Auction: .*2021/ }).isVisible();
+    var inView = await page.getByRole('link', { name: /.*Cheap Coffee Ended BIN Highest Bid: 6[,\.]000.* Highest Own: 6[,\.]000.* End of Auction: .*2021/ }).isVisible();
     if (inView)
       break;
     await page.waitForTimeout(50);
   }
-  await page.getByRole('link', { name: /.*Cheap Coffee Ended BIN Highest Bid: 6[,\.]000.* Highest Own: 0.* End of Auction: .*2021/ }).click();
+  await page.getByRole('link', { name: /.*Cheap Coffee Ended BIN Highest Bid: 6[,\.]000.* Highest Own: 6[,\.]000.* End of Auction: .*2021/ }).click();
 });
 
 async function switchToBids(page: Page) {
