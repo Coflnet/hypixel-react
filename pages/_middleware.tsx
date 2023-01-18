499c6eeff1b62857df3476e6c37fd25d8fc18fe2
matthias-luger <58751503+matthias-luger@users.noreply.github.com>
2023-01-18 22:07:11 +0100

    add rate limiting (#1030)
    * add rate limiting
    
    * re-add logging
    
    * change to 100 requests per minute

---
+++ pages/_middleware.tsx
@@ -0,0 +1,22 @@
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
+import rateLimit from '../utils/RateLimitUtils'
+import requestIp from 'request-ip'
+
+const limiter = rateLimit({
+    interval: 60 * 1000, // 60 seconds
+    uniqueTokenPerInterval: 500 // Max 500 users per second
+})
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+    try {
+        const clientIp = requestIp.getClientIp(req)
+        console.log('------------------')
+        console.log(clientIp)
+        await limiter.check(100, clientIp) // 100 requests per minute
+        return NextResponse.next()
+    } catch {
+        return new NextResponse('Rate limit exceeded', {
+            status: 429
+        })
+    }
+}
