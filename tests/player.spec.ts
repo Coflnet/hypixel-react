c0dec9707d7c096f9a7db1802a9b42e9df276eb7
Pennacap <64080101+pennacap@users.noreply.github.com>
2023-02-09 19:20:09 +0530

    fixed test using sky.coflnet instead of root url

--- tests/player.spec.ts
+++ tests/player.spec.ts
@@ -1,13 +1,13 @@
-import { test, expect, Page } from '@playwright/test';
+import { expect, Page, test } from '@playwright/test';
 
 test('Player auction opens', async ({ page }) => {
-  await page.goto('https://sky.coflnet.com/player/b876ec32e396476ba1158438d83c67d4');
+  await page.goto('/player/b876ec32e396476ba1158438d83c67d4');
   await page.getByRole('link', { name: /.*\[MAYOR\] Technoblade Ended Highest Bid: 400[,\.]000[,\.]000.* Starting Bid: 10.* End of Auction: .*/i }).click();
   await expect(page).toHaveURL(/.*\/auction\/73137bc47df84d31a9d8b010078ada0f/i);
 });
 
 test('Opens last bid', async ({ page }) => {
-  await page.goto('https://sky.coflnet.com/player/b876ec32e396476ba1158438d83c67d4');
+  await page.goto('/player/b876ec32e396476ba1158438d83c67d4');
   await switchToBids(page);
   await page.getByRole('link', { name: /.*Jingle Bells Ended BIN Highest Bid: 620[,\.]000.* Highest Own: 620[,\.]000.* End of Auction: .*/i }).click();
   await expect(page).toHaveURL(/.*\/auction\/c5ce8b40320b4b178e53cdfb746d8953/i);
@@ -15,7 +15,7 @@ test('Opens last bid', async ({ page }) => {
 
 test('Scroll down to older bid', async ({ page, isMobile, browserName }) => {
   test.skip(browserName === 'webkit' && isMobile, 'Scrolling is not supported on mobile safari');;
-  await page.goto('https://sky.coflnet.com/player/b876ec32e396476ba1158438d83c67d4');
+  await page.goto('/player/b876ec32e396476ba1158438d83c67d4');
   await switchToBids(page);
   for (let i = 0; i < 500; i++) {
     await page.mouse.wheel(0, 5000);
