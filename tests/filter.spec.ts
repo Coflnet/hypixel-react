b97c38f7ea3331508e3f59a99c8ffced5f5c666a
Matthias Luger <malua007@live.de>
2023-03-14 17:19:00 +0100

    maybe fix tests?

--- tests/filter.spec.ts
+++ tests/filter.spec.ts
@@ -1,14 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test('test', async ({ page }) => {
-  await page.goto('/item/ASPECT_OF_THE_DRAGON');
-  await page.getByRole('link', { name: 'Add Filter' }).click();
-  await page.getByPlaceholder('Add filter').fill('shar');
-  await page.getByRole('option', { name: 'sharpness' }).click();
-  await page.locator('form').filter({ hasText: 'sharpness01234567' }).getByRole('textbox').click();
-  await page.locator('form').filter({ hasText: 'sharpness01234567' }).getByRole('textbox').fill('5');
-  await page.locator('text=/ended.*ago/i').first().click();
-  await expect(page).toHaveURL(/.*\/auction\/.*/i);
-  // realy has sharpness 5
-  await page.getByText('Sharpness 5').click();
-});
+    await page.goto('/item/ASPECT_OF_THE_DRAGON')
+    await page.getByRole('link', { name: 'Add Filter' }).click()
+    await page.getByPlaceholder('Add filter').fill('shar')
+    await page.getByRole('option', { name: 'sharpness' }).click()
+    await page.locator('form').filter({ hasText: 'sharpness01234567' }).getByRole('textbox').click()
+    await page.locator('form').filter({ hasText: 'sharpness01234567' }).getByRole('textbox').fill('5')
+    await page.locator('text=/ended.*ago/i').first().click({ force: true })
+    await expect(page).toHaveURL(/.*\/auction\/.*/i)
+    // realy has sharpness 5
+    await page.getByText('Sharpness 5').click()
+})
