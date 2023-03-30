c7ce88effe0198cdcede510461de702deb7f28a1
Matthias Luger <malua007@live.de>
2023-03-30 15:47:21 +0200

    dont store videos of tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,8 +38,7 @@ const config: PlaywrightTestConfig = {
         baseURL: 'http://localhost:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry',
-        video: 'on'
+        trace: 'on-first-retry'
     },
 
     /* Configure projects for major browsers */
