899d0ef48699d44c06d639889cf9851ca60433d4
Matthias Luger <malua007@live.de>
2023-04-03 03:45:11 +0200

    skip sidebar in test

--- tests/crafts.spec.ts
+++ tests/crafts.spec.ts
@@ -2,10 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('Profitable craft page works', async ({ page, browserName }) => {
   test.skip(browserName === 'firefox', 'Still working on it');
-  await page.goto('/');
-  await page.getByTestId('MenuIcon').click();
-  await page.getByRole('link', { name: 'Profitable Crafts' }).dblclick();
-  await expect(page).toHaveURL(/.*\/crafts.*/i)
+  await page.goto('/crafts');
   await page.getByText('The top 3 crafts can only be seen with starter premium or betterYou Cheated the ').first().isVisible();
   await page.getByRole('button', { name: /.*Crafting Cost: .*/ }).first().click();
   await page.getByRole('heading', { name: 'Recipe' }).isVisible();
