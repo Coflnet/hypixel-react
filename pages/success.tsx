a5db591c5a6895bcd105fc263b6a5ca34808bb75
Matthias Luger <malua007@live.de>
2023-05-18 19:09:57 +0200

    add confetti animation to success page

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,30 +1,35 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Container } from 'react-bootstrap'
 import NavBar from '../components/NavBar/NavBar'
-import { getHeadElement } from '../utils/SSRUtils'
+import { getHeadElement, isClientSideRendering } from '../utils/SSRUtils'
+import ReactConfetti from 'react-confetti'
 
 function Success() {
+    let [isSSR, setIsSSR] = useState(true)
+
+    useEffect(() => {
+        setIsSSR(false)
+    }, [])
     return (
         <div className="page">
             {getHeadElement('Payment successful')}
             <Container>
-                <h2 style={{ color: 'lime' }}>
+                <h2>
                     <NavBar />
-                    Payment successful
+                    <span style={{ color: 'lime' }}>Payment successful</span>
                 </h2>
                 <p>Your payment is being handled securely by our payment provider.</p>
                 <p>
                     It may take <b>a few minutes</b> until your CoflCoins are credited.
                 </p>
                 <Link href="/premium" className="disableLinkStyle">
-
                     <Button>Return to the Premium page</Button>
-
                 </Link>
             </Container>
+            {!isSSR ? <ReactConfetti width={window.innerWidth} height={window.innerHeight} recycle={false} /> : null}
         </div>
-    );
+    )
 }
 
 export default Success
