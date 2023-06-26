4fec5f5ae88679643d474825c6f7178c7e910eee
Matthias Luger <malua007@live.de>
2023-06-26 19:04:50 +0200

    fix test

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from './fixture'
 test('search player technoblade with special player search query', async ({ page }) => {
     await page.goto('/')
     await page.getByPlaceholder('Search player/item').click()
-    await page.getByPlaceholder('Search player/item').fill('playertechnoblade')
+    await page.getByPlaceholder('Search player/item').fill('player technoblade')
     await page.getByPlaceholder('Search player/item').press('Enter')
     await expect(page).toHaveURL('/player/b876ec32e396476ba1158438d83c67d4')
 })
