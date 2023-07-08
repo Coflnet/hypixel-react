37239b81915fb9508da10d7340b51fdf6c6744c8
Matthias Luger <malua007@live.de>
2023-07-08 23:51:05 +0200

    increase test delay

--- tests/fixture.ts
+++ tests/fixture.ts
@@ -3,7 +3,7 @@ import { test as base } from '@playwright/test'
 export const test = base.extend({
     page: async ({ page }, use) => {
         // timeout to stretch out the tests for api limit
-        await sleep(10000)
+        await sleep(11000)
         await page.route('**/*.{png,jpg,jpeg}', route => route.abort())
         await page.route('**/_next/image**', route => route.abort())
         await use(page)
