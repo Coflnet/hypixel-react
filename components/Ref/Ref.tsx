c394c525a837da1cd35b204ee4e958f7510df1c4
Matthias Luger <malua007@live.de>
2023-07-25 12:29:20 +0200

    update ref page

--- components/Ref/Ref.tsx
+++ components/Ref/Ref.tsx
@@ -68,10 +68,10 @@ function Ref() {
                         <p>Rewards per invited person:</p>
                         <ul>
                             <li>
-                                You receive <b>300 CoflCoins</b>
+                                You receive <b>200 CoflCoins</b>
                             </li>
                             <li>
-                                The invited person gets <b>3 days of premium</b> to test our services
+                                The invited person gets <b>2 days of premium</b> to test our services
                             </li>
                             <li>
                                 The first time a invited person buys CoflCoins, you get <b>25%</b> of the purchased amount
