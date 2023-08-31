f5742c4c769af87af0fd4dcdd29387b974de909e
Matthias Luger <malua007@live.de>
2023-08-31 14:33:35 +0200

    add workbox expiration of 1 year

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-useless-escape */
 /* eslint-disable no-undef */
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js')
+
+const CACHE_EXPIRATION_ONE_YEAR = 60 * 60 * 24 * 365
 
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here. Other Firebase libraries
@@ -9,119 +11,142 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-
 workbox.setConfig({
     debug: false
-});
+})
 
 // cache js files
 workbox.routing.registerRoute(
-    new RegExp('.*\.js$'),
+    new RegExp('.*.js$'),
     workbox.strategies.cacheFirst({
         cacheName: 'workbox:js',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: CACHE_EXPIRATION_ONE_YEAR
+            })
+        ]
     })
-);
+)
 
 // cache css files
 workbox.routing.registerRoute(
     // Cache CSS files
-    new RegExp('.*\.css$'),
+    new RegExp('.*.css$'),
     // Use cache but update in the background ASAP
     workbox.strategies.cacheFirst({
         // Use a custom cache name
         cacheName: 'workbox:css',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: CACHE_EXPIRATION_ONE_YEAR
+            })
+        ]
     })
-);
+)
 
 // cache generall images
 workbox.routing.registerRoute(
-    new RegExp('.*\.(?:png|jpg|jpeg|svg|gif)$'),
+    new RegExp('.*.(?:png|jpg|jpeg|svg|gif)$'),
     workbox.strategies.staleWhileRevalidate({
         // Use a custom cache name
         cacheName: 'workbox:image',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: CACHE_EXPIRATION_ONE_YEAR
+            })
+        ]
     })
-);
+)
 
 // cache .ico files
 workbox.routing.registerRoute(
-    new RegExp('.*\.ico$'),
+    new RegExp('.*.ico$'),
     workbox.strategies.staleWhileRevalidate({
         // Use a custom cache name
         cacheName: 'workbox:image',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: CACHE_EXPIRATION_ONE_YEAR
+            })
+        ]
     })
-);
+)
 
 // cache images from sky.lea.moe and craftatar
 workbox.routing.registerRoute(
     // Cache image files
-    new RegExp('(.*sky.lea.moe.*|.*crafatar.com\/avatars.*)'),
+    new RegExp('(.*sky.lea.moe.*|.*crafatar.com/avatars.*)'),
     // Use the cache if it's available
     workbox.strategies.staleWhileRevalidate({
         // Use a custom cache name
         cacheName: 'workbox:image',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: CACHE_EXPIRATION_ONE_YEAR
+            })
+        ]
     })
-);
+)
 
-importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-messaging.js')
 
 // Your web app's Firebase configuration
 let firebaseConfig = {
-    apiKey: "AIzaSyB1yFUo__ZzeTBKw7KRQNHIyhxL7q9cLdI",
-    authDomain: "skyblock-300817.firebaseapp.com",
-    projectId: "skyblock-300817",
-    storageBucket: "skyblock-300817.appspot.com",
-    messagingSenderId: "570302890760",
-    appId: "1:570302890760:web:60cd30b3753f747d6c62bd"
-};
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
+    apiKey: 'AIzaSyB1yFUo__ZzeTBKw7KRQNHIyhxL7q9cLdI',
+    authDomain: 'skyblock-300817.firebaseapp.com',
+    projectId: 'skyblock-300817',
+    storageBucket: 'skyblock-300817.appspot.com',
+    messagingSenderId: '570302890760',
+    appId: '1:570302890760:web:60cd30b3753f747d6c62bd'
+}
+firebase.initializeApp(firebaseConfig)
+const messaging = firebase.messaging()
 messaging.usePublicVapidKey('BESZjJEHTRUVz5_8NW-jjOToWiSJFZHDzK9AYZP6No8cqGHkP7UQ_1XnEPqShuQtGj8lvtjBlkfoV86m_PadW30')
-messaging.onBackgroundMessage(function(payload) {
-    const request = indexedDB.open('keyval-store', 1);
-    let db;
-    if (payload.data.type != "auction") {
-        return;
+messaging.onBackgroundMessage(function (payload) {
+    const request = indexedDB.open('keyval-store', 1)
+    let db
+    if (payload.data.type != 'auction') {
+        return
     }
 
-    let auction = JSON.parse(payload.data.auction);
+    let auction = JSON.parse(payload.data.auction)
 
-
-    let key = `auctionDetails\"${auction.uuid}\"`;
+    let key = `auctionDetails\"${auction.uuid}\"`
     let data = {
-        expireTimeStamp: (new Date()).getTime() + 60 * 1000,
+        expireTimeStamp: new Date().getTime() + 60 * 1000,
         response: auction
     }
 
-    request.onsuccess = function() {
-        db = this.result;
+    request.onsuccess = function () {
+        db = this.result
 
-        db.transaction('keyval', "readwrite")
-            .objectStore('keyval')
-            .add(JSON.stringify(data), key);
-    };
-});
+        db.transaction('keyval', 'readwrite').objectStore('keyval').add(JSON.stringify(data), key)
+    }
+})
 
 //Code for adding event on click of notification
-self.addEventListener('notificationclick', function(event) {
-    let url = event.notification.data.url;
-    event.notification.close();
+self.addEventListener('notificationclick', function (event) {
+    let url = event.notification.data.url
+    event.notification.close()
     event.waitUntil(
-        clients.matchAll({
-            type: 'window'
-        }).then(windowClients => {
-            // Check if there is already a window/tab open with the target URL
-            for (let i = 0; i < windowClients.length; i++) {
-                let client = windowClients[i];
-                // If so, just focus it.
-                if (client.url === url && 'focus' in client) {
-                    return client.focus();
+        clients
+            .matchAll({
+                type: 'window'
+            })
+            .then(windowClients => {
+                // Check if there is already a window/tab open with the target URL
+                for (let i = 0; i < windowClients.length; i++) {
+                    let client = windowClients[i]
+                    // If so, just focus it.
+                    if (client.url === url && 'focus' in client) {
+                        return client.focus()
+                    }
                 }
-            }
-            // If not, then open the target URL in a new window/tab.
-            if (clients.openWindow) {
-                return clients.openWindow(url);
-            }
-        })
-    );
-});
+                // If not, then open the target URL in a new window/tab.
+                if (clients.openWindow) {
+                    return clients.openWindow(url)
+                }
+            })
+    )
+})
