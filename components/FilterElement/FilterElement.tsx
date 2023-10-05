2949a1f8d1ab2378af293ee1dbe545ab37557e79
Matthias Luger <malua007@live.de>
2023-10-05 02:33:50 +0200

    remove console.log

--- components/FilterElement/FilterElement.tsx
+++ components/FilterElement/FilterElement.tsx
@@ -31,7 +31,6 @@ function FilterElement(props: Props) {
     let [errorText, setErrorText] = useState('')
 
     useEffect(() => {
-        console.log(props)
         if (value) {
             return
         }
