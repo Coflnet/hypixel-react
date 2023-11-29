53c41ab85d7e7bb5813f439d74596242a34ca087
jason-tung <jasontungnyc@gmail.com>
2023-11-29 18:24:31 -0500

    more bl

--- utils/BMParser.tsx
+++ utils/BMParser.tsx
@@ -36,18 +36,22 @@ export const BMConvert = (
         restrictions: DEFAULT_FLIP_SETTINGS.RESTRICTIONS
     }
     // true_blacklist -> ForceBlacklist
-    let tags: string[] = []
     for (const TBLEntry of input.true_blacklist) {
-        tags.push(TBLEntry)
+        output.restrictions.push({
+            type: 'blacklist',
+            item: { name: TBLEntry } as Item,
+            itemFilter: {
+                ForceBlacklist: 'true'
+            }
+        })
     }
-    output.restrictions.push({
-        type: 'blacklist',
-        tags: [...tags],
-        itemFilter: {
-            ForceBlacklist: 'true'
-        }
-    })
-    tags = []
+    // regular blacklist
+    for (const BLEntry of input.blackist) {
+        output.restrictions.push({
+            type: 'blacklist',
+            item: { name: BLEntry } as Item
+        })
+    }
     // filter.minProfit = getNumberFromShortenString(json.thresholds.threshold.threshold)
     // filter.minProfitPercent = json.thresholds.threshold.threshold_percentage * 100
     // filter.maxCost = getNumberFromShortenString(json.thresholds.threshold.max_cost)
