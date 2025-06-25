81a923d3895212b5536997cb43e17daac813a192
Matthias Luger <malua007@live.de>
2025-06-25 16:12:18 +0200

    add info for skin filters

--- components/ItemFilter/ItemFilterPropertiesDisplay.tsx
+++ components/ItemFilter/ItemFilterPropertiesDisplay.tsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from 'react'
 import api from '../../api/ApiHelper'
 import { camelCaseToSentenceCase, convertTagToName, numberWithThousandsSeparators } from '../../utils/Formatter'
 import { useForceUpdate } from '../../utils/Hooks'
+import Tooltip from '../Tooltip/Tooltip'
+import HelpIcon from '@mui/icons-material/Help'
 
 interface Props {
     filter?: ItemFilter
@@ -15,6 +17,8 @@ interface Props {
 
 const DATE_FORMAT_FILTER = ['EndBefore', 'EndAfter', 'ItemCreatedBefore', 'ItemCreatedAfter']
 const SELLER_FORMAT_FILTER = 'Seller'
+const SKIN_FILTER = 'Skin'
+const PET_SKIN_FILTER = 'PetSkin'
 
 function ItemFilterPropertiesDisplay(props: Props) {
     let [localFilter, setLocalFilter] = useState(props.filter || {})
@@ -70,6 +74,7 @@ function ItemFilterPropertiesDisplay(props: Props) {
                     }
 
                     let display = convertTagToName(localFilter[key])
+                    let info: string | null = null;
 
                     if (key === 'ItemNameContains') {
                         display = localFilter[key]
@@ -110,6 +115,11 @@ function ItemFilterPropertiesDisplay(props: Props) {
                         display = localFilter._sellerName
                     }
 
+                    // Special case for skin filter
+                    if (key === SKIN_FILTER || key === PET_SKIN_FILTER) {
+                        info = 'This filter only works on applied skins on pets/armor. For the items there is the "ItemCategory" filter.';
+                    }
+
                     if (!localFilter[key] && !display) {
                         display = '-'
                     }
@@ -118,7 +128,13 @@ function ItemFilterPropertiesDisplay(props: Props) {
                         <span key={key} >
                             <div className="ellipse mb-2" title={display}>
                                 {camelCaseToSentenceCase(key)}: {display}
+                                {info ? <Tooltip
+                                    type="hover"
+                                    content={<HelpIcon style={{ color: '#007bff', cursor: 'pointer', marginLeft: 5 }} />}
+                                    tooltipContent={<span>{info}</span>}
+                                /> : null}
                             </div>
+
                             {props.isEditable ? (
                                 <span
                                     style={{ color: 'red', cursor: 'pointer' }}
