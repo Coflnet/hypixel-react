c0dbcb33d1f39ec892335a43e463092ca995ea90
Äkwav <ekwav@coflnet.com>
2025-07-25 12:40:28 +0200

    change wiki link to main page again

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -182,7 +182,7 @@ function NavBar(props: Props) {
                             <MenuItem className={styles.menuItem} component={<Link href={'/about'} />} icon={<PolicyIcon />}>
                                 Links / Legal
                             </MenuItem>
-                            <MenuItem className={styles.menuItem} component={<Link href={'/wiki/mod'} />} icon={<ChatIcon />}>
+                            <MenuItem className={styles.menuItem} component={<Link href={'/wiki'} />} icon={<ChatIcon />}>
                                 Wiki
                             </MenuItem>
                             <MenuItem
