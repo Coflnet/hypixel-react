1a46f79dea387d8c933c952e197a44ec733020cb
Äkwav <ekwav@coflnet.com>
2025-08-24 22:42:33 +0200

    update cypress and avoid fanky crafts issue

---
+++ cypress/e2e/crafts.cy.ts
@@ -0,0 +1,15 @@
+describe('Profitable craft page', () => {
+    afterEach(() => {
+        // Prevents running into the rate limit
+        cy.wait(10000)
+    })
+    it('works', () => {
+        cy.visit('/crafts')
+        cy.contains('The top 3 crafts can only be seen with starter premium or betterYou Cheated the ').should('be.visible')
+        // click the element twice to avoid flakyness
+        cy.contains('button', 'Crafting Cost').scrollIntoView().children().first().should('be.visible').click()
+        cy.contains('button', 'Crafting Cost').scrollIntoView().children().first().should('be.visible').click()
+        cy.contains('h3', 'Recipe').should('be.visible')
+        cy.contains(/\)[\.,\d]* Coins.*/).should('be.visible')
+    })
+})
