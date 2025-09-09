bb0edfd13405afc508f5ae554a9a775810a9d043
Matthias Luger <malua007@live.de>
2025-09-09 23:18:47 +0200

    implement npc flips

---
+++ app/npc/page.tsx
@@ -0,0 +1,31 @@
+import { getHeadMetadata } from '../../utils/SSRUtils'
+import NavBar from '../../components/NavBar/NavBar'
+import { Container } from 'react-bootstrap'
+import { getQueryClient } from '../../utils/QueryUtils'
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+import { getApiFlipNpc, getGetApiFlipNpcQueryKey } from '../../api/_generated/skyApi'
+import { NpcFlips } from '../../components/NpcFlips/NpcFlips'
+
+export default async function Page() {
+    const queryClient = getQueryClient()
+    queryClient.prefetchQuery({
+        queryKey: [getGetApiFlipNpcQueryKey()],
+        queryFn: () => getApiFlipNpc(),
+    })
+    return (
+        <>
+            <Container>
+                <h2>
+                    <NavBar />
+                    NPC Flips
+                </h2>
+                <hr />
+                <HydrationBoundary state={dehydrate(queryClient)}>
+                    <NpcFlips />
+                </HydrationBoundary>
+            </Container>
+        </>
+    )
+}
+
+export const metadata = getHeadMetadata('NPC Flips', 'Discover profitable Hypixel SkyBlock NPC flipping opportunities. Buy from NPCs and sell on the Auction House or Bazaar for a profit.', undefined, ['npc', 'flips', 'hypixel', 'skyblock', 'flip', 'npc flips', 'npc flipper'])
