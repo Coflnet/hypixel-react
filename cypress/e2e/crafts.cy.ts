a30a3be53d7615e2561cfe73fa605f7cb4ac2349
Matthias Luger <malua007@live.de>
2025-09-10 19:29:22 +0200

    maybe fix test

--- cypress/e2e/crafts.cy.ts
+++ cypress/e2e/crafts.cy.ts
@@ -6,7 +6,7 @@ describe('Profitable craft page', () => {
     it('works', { defaultCommandTimeout: 10000 }, () => {
         cy.visit('/crafts')
         cy.contains('The top 3 crafts can only be seen with starter premium or betterYou Cheated the ').should('be.visible')
-        cy.contains('button', 'Crafting Cost').scrollIntoView().children().last().scrollIntoView().should('be.visible').click()
+        cy.contains('button', 'Crafting Cost').last().click()
         cy.contains('h3', 'Recipe').should('be.visible')
         cy.contains(/\)[\.,\d]* Coins.*/).should('be.visible')
     })
