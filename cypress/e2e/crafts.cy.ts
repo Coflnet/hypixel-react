fb505a870f5cebe1b858cd8e0ea4f50d719ce67e
Matthias Luger <malua007@live.de>
2025-09-10 19:06:07 +0200

    mybe fix tests

--- cypress/e2e/crafts.cy.ts
+++ cypress/e2e/crafts.cy.ts
@@ -3,7 +3,7 @@ describe('Profitable craft page', () => {
         // Prevents running into the rate limit
         cy.wait(10000)
     })
-    it('works', () => {
+    it('works', { defaultCommandTimeout: 10000 }, () => {
         cy.visit('/crafts')
         cy.contains('The top 3 crafts can only be seen with starter premium or betterYou Cheated the ').should('be.visible')
         cy.contains('button', 'Crafting Cost').scrollIntoView().children().first().should('be.visible').click()
