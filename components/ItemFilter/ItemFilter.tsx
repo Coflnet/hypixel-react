fa341f0838d588d9adde8148fa9860d5c18c5257
Äkwav <ekwav@coflnet.com>
2025-09-16 23:50:02 +0200

    prevent focus change in item change as well really fix #1424

--- components/ItemFilter/ItemFilter.tsx
+++ components/ItemFilter/ItemFilter.tsx
@@ -16,6 +16,7 @@ import { btoaUnicode } from '../../utils/Base64Utils'
 import { ITEM_FILTER_USE_COUNT, LAST_USED_FILTER, getSettingsObject, setSetting } from '../../utils/SettingsUtils'
 import ModAdvert from './ModAdvert'
 import { isClientSideRendering } from '../../utils/SSRUtils'
+import { isAnySearchInputInUse } from '../../utils/SearchFocusUtils'
 import { usePathname, useRouter } from 'next/navigation'
 import option from '../PriceGraph/AuctionHousePriceGraph/PriceGraphConfig'
 
@@ -37,29 +38,6 @@ const groupedFilter = [
     ['Enchantment', 'EnchantLvl'],
     ['SecondEnchantment', 'SecondEnchantLvl']
 ]
-
-/**
- * Check if the global search bar is currently in use (has focus or contains text)
- * @returns true if the global search is being used, false otherwise
- */
-function isGlobalSearchInUse(): boolean {
-    if (typeof document === 'undefined') {
-        return false // Server-side rendering
-    }
-    
-    const globalSearchInput = document.getElementById('search-bar') as HTMLInputElement
-    if (!globalSearchInput) {
-        return false
-    }
-    
-    // Check if the search input has focus
-    const hasFocus = document.activeElement === globalSearchInput
-    
-    // Check if the search input contains text
-    const hasText = globalSearchInput.value && globalSearchInput.value.trim().length > 0
-    
-    return hasFocus || !!hasText
-}
 
 function ItemFilter(props: Props) {
     let router = useRouter()
@@ -425,12 +403,12 @@ function ItemFilter(props: Props) {
                                         id="add-filter-typeahead"
                                         autoFocus={
                                             props.autoSelect === undefined
-                                                ? Object.keys(getPrefillFilter(props.filters, props.ignoreURL, props.disableLastUsedFilter)).length === 0 && !isGlobalSearchInUse()
+                                                ? Object.keys(getPrefillFilter(props.filters, props.ignoreURL, props.disableLastUsedFilter)).length === 0 && !isAnySearchInputInUse()
                                                 : props.autoSelect
                                         }
                                         defaultOpen={
                                             props.autoSelect === undefined
-                                                ? Object.keys(getPrefillFilter(props.filters, props.ignoreURL, props.disableLastUsedFilter)).length === 0 && !isGlobalSearchInUse()
+                                                ? Object.keys(getPrefillFilter(props.filters, props.ignoreURL, props.disableLastUsedFilter)).length === 0 && !isAnySearchInputInUse()
                                                 : props.autoSelect
                                         }
                                         ref={typeaheadRef}
