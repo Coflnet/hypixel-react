c856ea310a229edea96c3cad7f35b06c7b5e995e
Ekwav <ekwav@users.noreply.github.com>
2026-05-24 11:41:50 +0200

    Polish FusionFlips selling step

--- components/FusionFlips/FusionFlips.tsx
+++ components/FusionFlips/FusionFlips.tsx
@@ -246,7 +246,7 @@ export function FusionFlips() {
                     <li>Pick a fusion flip that has a positive profit from the list below.</li>
                     <li>Create a buy order for the displayed amount of each shard.</li>
                     <li>Go to the fusion machine on galatea, select first one then the other shard and fuse them.</li>
-                    <li>Create a sell order on the bazaar with the resulting shards</li>
+                    <li>Create a sell order on the bazaar with the resulting shards.</li>
                     <li>Repeat while margins and volume stay strong.</li>
                 </ol>
             </details>
